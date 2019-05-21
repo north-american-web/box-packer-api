7b1a2734be7728a25b42261b2bc214358ccfdc14
David Kidd <dkidd@northamericanweb.com>
2019-05-20 20:46:51 -0500

    Update to nawebco/boxpacker:^2.0.0

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,61 @@ use NAWebCo\BoxPacker\Packer;
 
 class Controller extends BaseController
 {
+    public function packing_attempt(Request $request)
+    {
+        $this->validate($request, [
+            'boxes' => 'required|array',
+            'boxes.*' => 'required|array',
+            'boxes.*.width' => 'required|numeric|gt:0',
+            'boxes.*.length' => 'required|numeric|gt:0',
+            'boxes.*.height' => 'required|numeric|gt:0',
+            'boxes.*.description' => 'nullable|between:1,128',
+            'items' => 'required|array',
+            'items.*' => 'required|array',
+            'items.*.width' => 'required|numeric|gt:0',
+            'items.*.length' => 'required|numeric|gt:0',
+            'items.*.height' => 'required|numeric|gt:0',
+            'items.*.description' => 'nullable|between:1,128',
+        ]);
+
+        $packer = new Packer();
+
+        foreach( $request->boxes as $boxData ){
+            $packer->addBox(PackingItemsFactory::buildContainer($boxData));
+        }
+
+        foreach( $request->items as $itemData ){
+            $packer->addItem(PackingItemsFactory::buildSolid($itemData));
+        }
+
+        try {
+            $result = $packer->pack();
+        } catch( \Exception $e){
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        return response()->json([
+            'success' => $result->success(),
+            'packed' => $this->normalizeBoxData($result->getPackedBoxes(true)),
+            'empty' => $this->normalizeBoxData($result->getEmptyBoxes(true)),
+            'leftOverItems' => $result->getNotPackedItems()
+        ]);
+    }
+
+    protected function normalizeBoxData($boxesData)
+    {
+        $normalized = [];
+
+        foreach( $boxesData as $boxData ){
+            $normalizedBoxData = $boxData['box']->toArray();
+            $normalizedBoxData['contents'] = $boxData['contents'];
+            $normalized[] = $normalizedBoxData;
+        }
+
+        return $normalized;
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,45 +19,4 @@ $router->get('/', function () use ($router) {
     return 'Documentation: <a href="https://github.com/north-american-web/box-packer-api">https://github.com/north-american-web/box-packer-api</a>';
 });
 
-$router->post('/v1/packing_attempt', function (Request $request) {
-    $this->validate($request, [
-        'boxes' => 'required|array',
-        'boxes.*' => 'required|array',
-        'boxes.*.width' => 'required|numeric|gt:0',
-        'boxes.*.length' => 'required|numeric|gt:0',
-        'boxes.*.height' => 'required|numeric|gt:0',
-        'boxes.*.description' => 'nullable|between:1,128',
-        'items' => 'required|array',
-        'items.*' => 'required|array',
-        'items.*.width' => 'required|numeric|gt:0',
-        'items.*.length' => 'required|numeric|gt:0',
-        'items.*.height' => 'required|numeric|gt:0',
-        'items.*.description' => 'nullable|between:1,128',
-    ]);
-
-    $packer = new Packer();
-
-    foreach( $request->boxes as $boxData ){
-        $packer->addContainer(PackingItemsFactory::buildContainer($boxData));
-    }
-
-    foreach( $request->items as $itemData ){
-        $packer->addItem(PackingItemsFactory::buildSolid($itemData));
-    }
-
-    try {
-        $result = $packer->pack();
-    } catch( \Exception $e){
-        return response()->json([
-            'success' => false,
-            'message' => $e->getMessage()
-        ]);
-    }
-
-    return response()->json([
-        'success' => $result->success(),
-        'packed' => $result->getPackedContainers(),
-        'empty' => $result->getEmptyContainers(),
-        'leftOverItems' => $result->getNotPackedItems()
-    ]);
-});
+$router->post('/v1/packing_attempt', 'Controller@packing_attempt');
